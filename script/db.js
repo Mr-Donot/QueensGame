c300d9f1c39b9bb590677315b10966148e14334b
Mr-Donot <florian.daunay37@gmail.com>
2024-05-21 19:51:30 +0200

    insert in db test 2

--- script/db.js
+++ script/db.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-analytics.js";
+import { getDatabase, ref, set, get} from "https://www.gstatic.com/firebasejs/10.12.1/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
