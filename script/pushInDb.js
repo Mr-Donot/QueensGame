858cbe14ee4e403308b0ee299067ff225868df18
Mr-Donot <florian.daunay37@gmail.com>
2024-05-21 22:21:18 +0200

    insert in db test 5

--- script/pushInDb.js
+++ script/pushInDb.js
@@ -2,12 +2,18 @@ import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.0.0/
 
 
 function saveWinInDB(){
+    var username = document.querySelector("#username").value;
+    if (username == ""){
+      console.log("entrez un nom !")
+      return;
+    }
     const db = getDatabase();
-    set(ref(db, 'wins/' + currentMap),
+    set(ref(db, 'user/' + username),
     {
       map: currentMap,
       timer:0,
     });
+    closePopup();
 }
 
-document.querySelector("#close-popup").addEventListener("click", saveWinInDB);
+document.querySelector("#save-button").addEventListener("click", saveWinInDB);
