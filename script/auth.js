b957586f01cb486d7988c36363491eebbfd14967
Mr-Donot <florian.daunay37@gmail.com>
2024-05-24 00:47:55 +0200

    update app to auth in import auth

--- script/auth.js
+++ script/auth.js
@@ -1,4 +1,4 @@
-import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 
 
 const auth = getAuth();
