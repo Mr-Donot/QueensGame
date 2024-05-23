255b5acf6014aa80c6d1768fedbf6eba889ede8e
Mr-Donot <florian.daunay37@gmail.com>
2024-05-24 01:09:44 +0200

    new test without db.js for auth

--- script/auth.js
+++ script/auth.js
@@ -1,5 +1,24 @@
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmqcJVph_jsTZVBCt2tChGFlqia6jB-o4",
+  authDomain: "test-queens-game.firebaseapp.com",
+  databaseURL: "https://test-queens-game-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "test-queens-game",
+  storageBucket: "test-queens-game.appspot.com",
+  messagingSenderId: "423803906034",
+  appId: "1:423803906034:web:d2804f284c0175874f2d6b",
+  measurementId: "G-9L5RKZ5P2R"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
 
 
 document.querySelector("#button_register").addEventListener("click", register);
@@ -13,7 +32,6 @@ function register(){
     if (!(validate_email(email) && validate_password(password) && validate_name(name))){
         return;
     }
-    const auth = getAuth();
     auth.createUserWithEmailAndPassword(email, password)
     .then(function(){
         let user = auth.currentUser;
@@ -41,7 +59,6 @@ function login() {
     if (!(validate_email(email) && validate_password(password))) {
       return
     }
-    const auth = getAuth(app);
     auth.signInWithEmailAndPassword(email, password)
     .then(function() {
       let user = auth.currentUser
