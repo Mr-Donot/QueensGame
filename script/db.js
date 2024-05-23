5c8cb5dd80ab7b6e74570de6c53dd39770e85f73
Mr-Donot <florian.daunay37@gmail.com>
2024-05-24 00:26:25 +0200

    move formatteddate function

--- script/db.js
+++ script/db.js
@@ -18,18 +18,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-function getFormattedDateTime() {
-
-  const now = new Date();
-
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-  const hour = String(now.getHours()).padStart(2, '0');
-  const minute = String(now.getMinutes()).padStart(2, '0');
-  const second = String(now.getSeconds()).padStart(2, '0');
-
-  const formattedDateTime = `${year}-${month}-${day}-${hour}-${minute}-${second}`;
-
-  return formattedDateTime;
-}
--- script/pushInDb.js
+++ script/pushInDb.js
@@ -68,6 +68,20 @@ async function saveNewData(timer) {
     }
 }
 
+function getFormattedDateTime() {
 
+    const now = new Date();
+  
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hour = String(now.getHours()).padStart(2, '0');
+    const minute = String(now.getMinutes()).padStart(2, '0');
+    const second = String(now.getSeconds()).padStart(2, '0');
+  
+    const formattedDateTime = `${year}-${month}-${day}-${hour}-${minute}-${second}`;
+  
+    return formattedDateTime;
+  }
 
 document.querySelector("#save-button").addEventListener("click", saveWinInDB);
