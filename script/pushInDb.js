030474f71dc4afbfb9197aa54172c630d36b5cc1
Mr-Donot <florian.daunay37@gmail.com>
2024-05-24 11:40:27 +0200

    test

--- script/pushInDb.js
+++ script/pushInDb.js
@@ -23,7 +23,7 @@ async function saveWinInDB() {
 
     var timer = document.querySelector("#chronometer").innerHTML;
     const db = getDatabase();
-    const userRef = ref(db, 'user/' + username + '/' + currentMap);
+    const userRef = ref(db, 'users/' + username + '/' + currentMap);
 
     try {
         const snapshot = await get(userRef);
@@ -74,7 +74,7 @@ async function saveNewData(timer) {
     const db = getDatabase();
     
     try {
-        await set(ref(db, 'user/' + username + '/' + currentMap), { "timer":timer, "date": getFormattedDateTime()});
+        await set(ref(db, 'users/' + username + '/' + currentMap), { "timer":timer, "date": getFormattedDateTime()});
         console.log("New timer saved successfully.");
         closePopup();
     } catch (error) {
